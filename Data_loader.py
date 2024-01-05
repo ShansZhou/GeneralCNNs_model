a48ab60498f2821348be3649a0c7e2d88c186ffe
ShansZhou <476993098@qq.com>
2024-01-05 18:39:13 +0800

    update data loader

--- Data_loader.py
+++ Data_loader.py
@@ -3,17 +3,22 @@ from torch.utils.data.dataloader import DataLoader
 import torchvision.transforms as transforms
 
 
+
+
 def loadMNIST():
     
+    train_transform = transforms.Compose([
+        transforms.Pad(padding=2, fill=0, padding_mode="constant"),
+        transforms.ToTensor()
+    ])
+    
     # download dataset
-    train_set = MNIST(root="../data/", train=True, transform=transforms.ToTensor(), download=True)
-    test_set = MNIST(root="../data/", train=False, transform=transforms.ToTensor())
+    train_set = MNIST(root="../data/", train=True, transform=train_transform, download=True)
+    test_set = MNIST(root="../data/", train=False, transform=train_transform)
     
     # load dataset
-    
     batch_size = 4
     train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
-    
     test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False)
     
     return train_loader, test_loader
